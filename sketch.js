740e0b7516b7f7954c99e083b9fbb72e8c16372b
xingyu yao <xy2779@nyu.edu>
2024-09-29 23:26:42 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,25 +1,19 @@
 function setup() {
   createCanvas(600, 800);
-  background(238,210,10);
+  background(238, 210, 10);
 
-
-  for (let d = 15;d < 600;d += 50){
-  for(let c = 0;c < 800;c += 50){
-    fill(15,15,15);
-    ellipse(d,c,20);
-  
-  }
-  }
-  for (let d = 40; d < 600; d+=50) {
-    for (let c = 30; c < 900; c+=50) {
+  for (let d = 15; d < 600; d += 50) {
+    for (let c = 0; c < 800; c += 50) {
       fill(15, 15, 15);
-      ellipse(d, c, 10, 10); 
-      
+      ellipse(d, c, 20);
+    }
+  }
+  for (let d = 40; d < 600; d += 50) {
+    for (let c = 30; c < 900; c += 50) {
+      fill(15, 15, 15);
+      ellipse(d, c, 10, 10);
     }
   }
 }
 
-function draw() {
-}
-
-
+function draw() {}
