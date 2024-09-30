128d9285a9c2cfa3cceefba08732b1e7afb75407
xingyu yao <xy2779@nyu.edu>
2024-09-29 23:28:18 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,15 @@
 function setup() {
-  createCanvas(600, 800);
+  createCanvas(windowWidth, windowHeight);
   background(238, 210, 10);
 
-  for (let d = 15; d < 600; d += 50) {
-    for (let c = 0; c < 800; c += 50) {
+  for (let d = 15; d < windowWidth; d += 50) {
+    for (let c = 0; c < windowHeight; c += 50) {
       fill(15, 15, 15);
       ellipse(d, c, 20);
     }
   }
-  for (let d = 40; d < 600; d += 50) {
-    for (let c = 30; c < 900; c += 50) {
+  for (let d = 40; d < windowWidth; d += 50) {
+    for (let c = 30; c < windowHeight; c += 50) {
       fill(15, 15, 15);
       ellipse(d, c, 10, 10);
     }
